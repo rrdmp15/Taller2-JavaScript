a22c678d5badbe7b2122cc0c33d41fcfb797ab7e
rrdmp15 <rdayismar@gmail.com>
2023-03-09 13:52:34 -0500

    feat: :rocket: Ejercicio 5
    Este programa define cual de los dos valores indicados es mayor y si el primero lo es imprime una suma y una resta co ambos valores y si el segundo es mayor, imprime una division y una ultiplicacion de ambos valores

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-let nombre1 = prompt("¿cual es tu nombre?");
+let numero1 = parseInt(prompt("Escribe el primer numero:"));
+let numero2 = parseInt(prompt("Escribe el segundo numero:"));
 
-let edad1 = parseInt(prompt("¿cual es tu edad?"));
+if (numero1 > numero2){
+    let suma = (numero1 + numero2)
+    let resta = (numero1 - numero2)
+    alert("La suma es igual a " + suma + " y la diferencia es igual a " + resta)
+} else if (numero1 < numero2){
+    let producto = (numero1 * numero2)
+    let dividendo = (numero1 / numero2)
 
-let nombre2 = prompt("¿cual es tu nombre?");
-
-let edad2 = parseInt(prompt("¿cual es tu edad?"));
-
-let nombre3 = prompt("¿cual es tu nombre?");
-
-let edad3 = parseInt(prompt("¿cual es tu edad?"));
-
-let edades = [edad1, edad2, edad3]
-
-Math.max(...edades)
-
-if(edad1 > edad2 && edad1 > edad3){
-    alert("La persona de mayor edad es " + nombre1);
-} else if (edad2 > edad1 && edad2 > edad3){
-    alert("La persona de mayor edad es " + nombre2);
-} else if (edad3 > edad1 && edad3 > edad2){
-    alert("La persona de mayor edad es " + nombre3);
+    alert("El producto es igual a " + producto + " y la division es igual a " + dividendo)
 }
