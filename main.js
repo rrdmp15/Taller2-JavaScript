32c8cd215dba14793da4948203d11ccbdeade4f5
rrdmp15 <rdayismar@gmail.com>
2023-03-09 13:18:21 -0500

    feat: :rocket: Ejercicio 3
    con este ejercicio podemos calcular el voltaje de un circuito co los datos de la resistencia y la intensidad d corriente

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-function repetir(){
-    let numero = parseInt(prompt("Escriba un numero:"))
+let intencidad = parseInt(prompt("Ingrese la intensidad de la corriente:"));
+let resistencia = parseInt(prompt("Ingrese la resistencia:"));
 
-    if(numero % 2 == 0 && numero > 10){
-        alert("Es par y mayor a 10");
-    } else if (numero % 2 == 0 && numero < 10){
-        alert("Es par y menor a 10");
-    } else if (numero % 2 == 0 && numero == 10){
-        alert("Es par");
-    }else if ( numero > 10){
-        alert("Es impar y mayor a 10");
-    }else if ( numero < 10){
-        alert("Es Impar y menor a 10");
-    }else if ( numero == 10){
-        alert("Es impar");
-    }else{
-        alert("Ingresa un numero valido");
-    }
+let voltaje = (intencidad * resistencia)
 
-    repetir()
-}
-
-repetir()
+alert("Tu voltaje es: " + voltaje)
