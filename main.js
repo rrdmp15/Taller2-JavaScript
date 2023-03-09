91526ae6bbbe05b4f40c0ab0da6443d61decce71
rrdmp15 <rdayismar@gmail.com>
2023-03-09 13:40:04 -0500

    feat: :rocket: Ejercicio 4
    Este ejercicio pide el nombre y la edad de tres personas y determina cual de las tres es la mayor

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-let intencidad = parseInt(prompt("Ingrese la intensidad de la corriente:"));
-let resistencia = parseInt(prompt("Ingrese la resistencia:"));
+let nombre1 = prompt("¿cual es tu nombre?");
 
-let voltaje = (intencidad * resistencia)
+let edad1 = parseInt(prompt("¿cual es tu edad?"));
 
-alert("Tu voltaje es: " + voltaje)
+let nombre2 = prompt("¿cual es tu nombre?");
+
+let edad2 = parseInt(prompt("¿cual es tu edad?"));
+
+let nombre3 = prompt("¿cual es tu nombre?");
+
+let edad3 = parseInt(prompt("¿cual es tu edad?"));
+
+let edades = [edad1, edad2, edad3]
+
+Math.max(...edades)
+
+if(edad1 > edad2 && edad1 > edad3){
+    alert("La persona de mayor edad es " + nombre1);
+} else if (edad2 > edad1 && edad2 > edad3){
+    alert("La persona de mayor edad es " + nombre2);
+} else if (edad3 > edad1 && edad3 > edad2){
+    alert("La persona de mayor edad es " + nombre3);
+}
